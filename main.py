7ee72a53f8ac2e9e942413f9c4de2e311c6fd9d9
chaudhariGaurav07 <gauravchaudhari7717@gmail.com>
2025-01-29 00:26:30 +0530

    added the google recognizer

--- main.py
+++ main.py
@@ -14,4 +14,22 @@ def speak(text):
 
 
 if __name__ == "__main__":
-    speak("heyy mihir how are you")
+    speak("Initializing Jarvis..")
+    while True:
+        # listen for the wake uo word "Jarvis"
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as sourse:
+            print("Listening...")
+            audio = r.listen(sourse)
+
+    
+
+        # recognize speech using google speech 
+        try:
+            command = r.recognize_google(audio)
+            print(command)
+        except sr.UnknownValueError:
+            print("google could not understand audio")
+        except sr.RequestError as e:
+            print("google error; {0}".format(e))
